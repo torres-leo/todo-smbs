eef105a47872e0c9d813bdd0a3fb286250ec04d2
torres-leo <leojts24@gmail.com>
2022-08-03 23:10:20 -0600

    making edite/delete

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,9 +9,13 @@
 			this.listTask = document.getElementById('containerTasks');
 			this.button = document.getElementById('buttonAdd');
 			this.input = document.getElementById('task');
+			this.buttonDel = document.getElementById('deleteButton');
+			this.buttonEdit = document.getElementById('editButton');
 		},
 		bindingEvents: function () {
 			this.button.addEventListener('click', this.addTask.bind(this));
+			this.buttonEdit?.addEventListener('click', this.editTask.bind(this));
+			this.buttonDel?.addEventListener('click', this.deleteTask.bind(this));
 		},
 		taskElement: function (taskValue) {
 			const listItem = document.createElement('li');
@@ -26,7 +30,10 @@
 			const deleteIcon = document.createElement('i');
 
 			containerIcons.classList.add('containerIcon');
+			editButton.setAttribute('id', 'editButton');
+			editButton.setAttribute('type', 'button');
 			editButton.classList.add('button', 'icon', 'edite');
+			deleteButton.setAttribute('id', 'deleteButton');
 			deleteButton.classList.add('button', 'icon', 'delete');
 			editIcon.classList.add('fa-solid', 'fa-pen-to-square');
 			deleteIcon.classList.add('fa-solid', 'fa-trash-can');
@@ -39,14 +46,40 @@
 			containerIcons.appendChild(editButton);
 			containerIcons.appendChild(deleteButton);
 
+			listItem.dataset.id;
 			listItem.appendChild(label);
 			listItem.appendChild(containerIcons);
 
-			return listItem;
+			const id = Date.now().toString();
+
+			this.list.push({ id, taskValue });
+			console.log(this.list);
 		},
-		addTask: function () {
-			this.taskElement(this.input.value);
+		addTask: function (e) {
+			e.preventDefault();
+
+			if (this.input.value === '') {
+				alert("can't send an empty task");
+			} else {
+				this.taskElement(this.input.value);
+			}
+			this.input.value = '';
+
+			const deleteButton = document.getElementById('deleteButton');
+			deleteButton.onclick = (e) => {
+				// console.log(e.target.parentElement.parentElement);
+				let li = e.target.parentElement.parentElement;
+				let ul = li.parentElement;
+
+				ul.removeChild(li);
+			};
+			// const editButton = document.getElementById('editButton');
+			// editButton.onclick = (e) => {
+			// 	console.log(e.target.parentElement.parentElement);
+			// };
 		},
+
+		editTask: function () {},
 	};
 	module.init();
 })();
