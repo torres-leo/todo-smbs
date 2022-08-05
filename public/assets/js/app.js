ec16e773bd7a6b0388e1833068c72c0ea80996cd
torres-leo <leojts24@gmail.com>
2022-08-04 23:27:40 -0600

    finish delete button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,17 +4,21 @@
 		init: function () {
 			this.cacheDom();
 			this.bindingEvents();
-			this.showTask();
+			this.showTasks();
 		},
+
 		cacheDom: function () {
 			this.listTask = document.getElementById('containerTasks');
 			this.button = document.getElementById('buttonAdd');
 			this.input = document.getElementById('task');
 		},
+
 		bindingEvents: function () {
 			this.button.addEventListener('click', this.addTask.bind(this));
 			this.listTask.addEventListener('click', this.deleteTask.bind(this));
+			this.listTask.addEventListener('click', this.editTask.bind(this));
 		},
+
 		taskElement: function (taskValue) {
 			const listItem = document.createElement('li');
 			listItem.classList.add('containerTasks-item');
@@ -22,16 +26,20 @@
 			const label = document.createElement('label');
 
 			const containerIcons = document.createElement('div');
+			const checkInput = document.createElement('input');
 			const editButton = document.createElement('button');
 			const deleteButton = document.createElement('button');
 			const editIcon = document.createElement('i');
 			const deleteIcon = document.createElement('i');
 
+			label.classList.add('text');
 			containerIcons.classList.add('containerIcon');
-			editButton.setAttribute('id', 'editButton');
+			checkInput.classList.add('checkItem');
+			checkInput.setAttribute('type', 'checkbox');
+			// editButton.setAttribute('id', 'editButton');
 			editButton.setAttribute('type', 'button');
 			editButton.classList.add('button', 'icon', 'edite');
-			deleteButton.setAttribute('id', 'deleteButton');
+			// deleteButton.setAttribute('id', 'deleteButton');
 			deleteButton.classList.add('button', 'icon', 'delete');
 			editIcon.classList.add('fa-solid', 'fa-pen-to-square');
 			deleteIcon.classList.add('fa-solid', 'fa-trash-can');
@@ -43,18 +51,23 @@
 			containerIcons.appendChild(editButton);
 			containerIcons.appendChild(deleteButton);
 
-			this.list.push(label.textContent);
-
-			this.list.forEach((element, index) => {});
-
+			listItem.appendChild(checkInput);
 			listItem.appendChild(label);
 			listItem.appendChild(containerIcons);
-			this.listTask.appendChild(listItem);
 
-			console.log(listItem);
+			this.list.push(label.textContent);
+
+			this.list.forEach((element, index) => {
+				editButton.setAttribute('id', index);
+				editIcon.setAttribute('id', index);
+				deleteButton.setAttribute('id', index);
+				deleteIcon.setAttribute('id', index);
+				listItem.setAttribute('id', index);
+			});
+			this.listTask.appendChild(listItem);
 			this.setTaskStorage(this.list);
 
-			// this.getTaskStorage('todoItems');
+			// console.log(this.list);
 		},
 		setTaskStorage: function (list) {
 			localStorage.setItem('todoItems', JSON.stringify(list));
@@ -83,32 +96,38 @@
 			console.log('Hola');
 		},
 		deleteTask: function (e) {
-			const elementLi = e.target.parentElement.parentElement;
-			const idElement = Number(elementLi.id);
-
-			console.log(idElement);
-
-			this.list.splice(idElement, 1);
-			this.setTaskStorage(this.list);
-			this.showTask();
+			let elementId = Number(e.target.id);
+			if (e.target.classList.contains('delete') || e.target.classList.contains('fa-trash-can')) {
+				// this.list.splice(elementId, 1);
+				let deleteItem = this.list.filter((element, index) => index !== elementId);
+				this.list = deleteItem;
+				this.setTaskStorage(this.list);
+				this.cleanHTML();
+				this.showTasks();
+			}
 		},
-		showTask: function () {
+		showTasks: function () {
 			this.getTaskStorage();
 			let li = '';
 			this.list.forEach((element, index) => {
-				li += `<li id=${index} class="containerTasks-item"><label>${element}</label>
+				li += `<li id=${index} class="containerTasks-item"><input type="checkbox" class="checkItem"><label class="text">${element}</label>
 				<div class="containerIcon">
-				<button id="editButton" class="button icon edite" type="button">
-				<i class="fa-solid fa-pen-to-square"></i>
+				<button id=${index} class="button icon edite" type="button">
+				<i class="fa-solid fa-pen-to-square" id=${index}></i>
 				</button>
-				<button id="deleteButton" class="button icon delete" type="button">
-				<i class="fa-solid fa-trash-can"></i>
+				<button id=${index} class="button icon delete" type="button">
+				<i class="fa-solid fa-trash-can" id=${index}></i>
 				</button>
 				</div>
 				</li>`;
 
 				this.listTask.innerHTML = li;
 			});
+		},
+		cleanHTML: function () {
+			while (this.listTask.firstChild) {
+				this.listTask.removeChild(this.listTask.firstChild);
+			}
 		},
 	};
 	module.init();
